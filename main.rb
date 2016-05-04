3843a46bd351f14dd6559a880b7a6604d83b74a9
Shawn Michaud <shawnmne@hotmail.com>
2016-05-04 16:13:53 -0500

    all done

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
-require "pry"
+#require "pry"
 require "sinatra"
 require "sinatra/activerecord"
 require "sinatra/reloader"
 
+
+ 
 Dir[File.dirname(__FILE__) + "/app/*/*.rb"].each { |file| require file}
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] ||
+	'postgres://localhost/week-five-project')
 
 set :views, Proc.new {File.join(root, "app/views/")}
 enable :method_override
